a736cfb31319ce77aafdb460a852e3c16cfd3f1f
JojoGuedel <67550456+JojoGuedel@users.noreply.github.com>
2023-03-20 07:07:45 +0100

    changes

--- src/hashmap.c
+++ src/hashmap.c
@@ -1,7 +1,43 @@
 #include "hashmap.h"
 
+#define HASHMAP_SPACE_RATIO 2
+
 typedef struct {
     const char* key;
     void* value;
     void* next;
 } Entry;
+
+static int _hash(const char* key, size_t size) {
+    int hash = 0;
+
+    for (int i = 0; key[i]; i++) {
+        hash += key[i];
+    }
+
+    return hash % size;
+}
+
+static void* _get_entry(void* key) {
+
+}
+
+Hashmap hashmap_create() {
+    return (Hashmap) {0, list_create()};
+}
+
+void hashmap_destroy(Hashmap* hashmap) {
+    list_destory(&hashmap->entries);
+}
+
+void hashmap_put(Hashmap* hashmap, const char* key, void* value) {
+    hashmap->size++;
+}
+
+void hashmap_get(Hashmap* hashmap, const char* key) {
+
+}
+
+void hashmap_remove(Hashmap* hashmap, const char* key) {
+
+}
--- src/hashmap.h
+++ src/hashmap.h
@@ -6,7 +6,14 @@
 typedef struct {
     size_t size;
 
-    List data;
+    List entries;
 } Hashmap;
+
+Hashmap hashmap_create();
+void hashmap_destroy(Hashmap* hashmap);
+
+void hashmap_put(Hashmap* hashmap, const char* key, void* value);
+void hashmap_get(Hashmap* hashmap, const char* key);
+void hashmap_remove(Hashmap* hashmap, const char* key);
 
 #endif
